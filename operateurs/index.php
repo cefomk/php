0d8e5054b02cbf6d0b00dc29d113cedfe9507fcf
Your Name <you@example.com>
2023-02-07 11:33:48 +0100

    Operateurs mathematiques

---
+++ operateurs/index.php
@@ -0,0 +1,55 @@
+<?php
+//phpinfo();
+
+// $x = 4;
+// $y = 2;
+// $z = 5;
+
+/* operatuers
++
+-
+*
+/
+**
+%
+*/
+
+//$calcul = $x % $z;
+// $calcul = ($x + $z) * $y;
+
+// echo $calcul;
+
+// $a = 5;
+// $b = 7;
+//$a = 23;
+// $a = '5';
+
+// $a += 6; // $a = $a + 6;
+// $a -= 6; // $a = $a - 6;
+// $a *= 6; // $a = $a * 6;
+// $a /= 6; // $a = $a / 6;
+// $a **= 6; // $a = $a ** 6;
+// $a %= 6; // $a = $a % 6;
+
+// echo $a;
+
+// $c = 'Hello ';
+// $c .= 'World.';
+// echo $c;
+
+// Crer 2 variables prixht et tva
+// Afficher le calcul du prix ttc
+
+$prixht = 34;
+$tva= 10;
+
+$prixttc = $prixht + ($tva * $prixht) /100;
+echo $prixttc . ' €';
+
+// Calculer l'âge d'une personnne en fonction 
+// de son année de naissance
+$anneeDeNaissance = 1990;
+
+
+// $age = ;
+
