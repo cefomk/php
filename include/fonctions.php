8503c7153c1bdbc9ea39b3cc3d67098a371275cc
Your Name <you@example.com>
2023-02-07 16:32:43 +0100

    Econce exercice fonctions calcul

--- include/fonctions.php
+++ include/fonctions.php
@@ -1,9 +1,13 @@
 <?php
-function calcul($a)
+function calcul($a,$b)
 {
-    $b = $a + 34;
-    return $b;
+    $c = $a + $b;
+    return $c;
     // echo 'test';
 }
 
-echo calcul(23);
+echo calcul(23,3);
+
+// Faire une fonction qui calul le Prix TTC en lui passant le prix HT et la TVA
+
+// Faire une fonction qui calcul l'âge d'une personne en donnant la date de naissance à la fonction
