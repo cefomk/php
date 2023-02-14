d97a769c4f6015800b176b6d83c06c0992057aff
Your Name <you@example.com>
2023-02-14 11:24:35 +0100

    Ajout travaux sur les tableaux

---
+++ travaux/travail-01.php
@@ -0,0 +1,29 @@
+<?php
+/*
+## Travail-01 : 
+
+- Réaliser un script qui convertit une température de degré Celsius °C en degré Fahrenheit °F 
+- Afficher les résultats dans un tableau html `table` , utiliser la fonction php `round` pour arrondir à l'unité supérieur
+- Voici le tableau de conversion que vous devez avoir :
+
+```
+| °C | °F |
+|--- |--- |
+| 25 | 77 |
+| 03 | 37.4 |
+| 35 | 95 |
+| 11 | 51.8 |
+
+```
+ */
+
+?>
+
+<style>
+table , th , td {
+	border:solid 1px black;
+}	
+td {
+	padding: .5rem;	
+}
+</style>
