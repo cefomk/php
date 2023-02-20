d55bb1be1b094e65cedd515b84d2ddb69cdce44c
formkdebvm <formk@kermed.net>
2023-02-20 19:29:43 +0100

    .

--- formulaire/traitement.php
+++ formulaire/traitement.php
@@ -4,10 +4,20 @@ include '../include/fonctions.php';
 //dd($_POST);
 // dd($_POST['competence']);
 
-echo 'Votre nom est : ' . $_POST['nom'] . '<br>';
+//echo 'Votre nom est : ' . $_POST['nom'] . '<br>';
 // echo 'Votre email est : ' . $_POST['email'] . '<br>';
 
-echo 'Les competences sont : ' . implode(',',$_POST['competence']);
+//echo 'Les competences sont : ' . implode(',',$_POST['competence']);
 
 // header('Location: http://localhost/php');
 // header('Location: /');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	
+		// the request method is fine
+
+} else {
+
+	exit('Invalid Request');
+
+}
